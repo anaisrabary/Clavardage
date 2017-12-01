33caf7f6db4b527ac64a43d80d3e5a91b4117a4c
anaisrabary <anais.rabary@mail.com>
2017-12-01 09:43:33 +0100

    TCP try bis

---
+++ src/main/java/com/DeRivasRabary/insa/network/AcceptClients.java
@@ -0,0 +1,39 @@
+package com.DeRivasRabary.insa.network;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class AcceptClients implements Runnable {
+    private ServerSocket serverSocket;
+    private IncomingMessageListener incomingMessageListener;
+    private Socket socket;
+
+    public AcceptClients(ServerSocket socket,IncomingMessageListener listener){
+        this.serverSocket = socket;
+        this.incomingMessageListener = listener;
+    }
+
+
+    @Override
+    public void run(){
+        Socket socket;
+        try {
+            while(true){
+                socket = this.serverSocket.accept(); //  block current thread until client asks for a connection
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                this.incomingMessageListener.onNewIncomingMessage(reader.readLine());
+
+                socket.close();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+
+}
--- src/main/java/com/DeRivasRabary/insa/network/TCPMessageReceiverManager.java
+++ src/main/java/com/DeRivasRabary/insa/network/TCPMessageReceiverManager.java
@@ -1,8 +1,15 @@
 package com.DeRivasRabary.insa.network;
 
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class TCPMessageReceiverManager implements MessageReceiverService {
     @Override
     public void listenOnPort(int port, IncomingMessageListener incomingMessageListener) throws Exception {
-        throw new UnsupportedOperationException();
+        ServerSocket serverSocket = new ServerSocket(port);
+        Thread accept = new Thread(new AcceptClients(serverSocket,incomingMessageListener));
+        accept.start();
+        //throw new UnsupportedOperationException();
     }
 }
