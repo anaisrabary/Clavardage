3aea10c4bfab8bd31e4fbd01c4529f887f06cc0b
Jean <derivas@etud.insa-toulouse.fr>
2017-12-06 17:53:07 +0100

    Modification de la ClasseTestReseau. Dedans se trouve la méthode qui peut récupérer l'adresse ip privée de notre pc. A externaliser dans Clavardage NI surement.

--- src/main/java/com/DeRivasRabary/insa/network/ClasseTestReseau.java
+++ src/main/java/com/DeRivasRabary/insa/network/ClasseTestReseau.java
@@ -1,20 +1,38 @@
 package com.DeRivasRabary.insa.network;
 
 
-import java.net.InetAddress;
+import java.net.*;
+import java.util.Enumeration;
 
 public class ClasseTestReseau {
 
-    public static void main(String[] args) {
-        try {
-            String ip = InetAddress.getLocalHost().getHostAddress();
-            String loopbackIp = InetAddress.getLoopbackAddress().getHostAddress();
-            System.out.println("IP : " + ip);
-            System.out.println("LoopBackIP : " + loopbackIp);
-        }
-        catch (Exception e){
-            e.printStackTrace();
+    /**
+     * Renvoie la première adresse IP privée trouvée du PC. Attention aux PC connectés sur plusieurs interfaces...
+     * A améliorer pour intégrer plusieurs interfaces. Mais fonctionne dans la majorité des cas.
+     * TODO : à déplacer ailleurs, c'est une classe temporaire
+     * @return String d'une ipprivée
+     */
+    public static String getLocalAdress() {
+        try
+        {
+            boolean trouve = false;
+            String ip = "" ;
+            Enumeration<NetworkInterface> e = NetworkInterface.getNetworkInterfaces();
+            while (!trouve || e.hasMoreElements()) {
+                Enumeration<InetAddress> i = e.nextElement().getInetAddresses();
+                while (i.hasMoreElements()) {
+                    InetAddress a = i.nextElement();
+                    if (a.isSiteLocalAddress()) {
+                        trouve = true;
+                        ip=a.getHostAddress();
+                    }
+                }
+            }
+            return ip ;
+        } catch (Exception e){
+            System.err.println("Erreur dans getLocalAdress. Impossible de trouver ip privée locale.\n" +
+                    "Etes vous bien connecté au réseau ?");
+            return "";
         }
     }
-
 }
